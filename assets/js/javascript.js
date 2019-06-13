4bd4a0f9f30c53263bc379008e2ebc39e10133c6
tyler <tylermarcus14@gmail.com>
2019-06-13 07:45:52 -0400

    fixed gif animations

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -16,11 +16,11 @@ $(document).ready(function () {
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
                 var gifDiv = $("<div>");
-                var gif = $("<img>").addClass("gif")
-                gif.attr("src", results[i].images.fixed_height.url);
-                gif.attr("data-still", results[i].images.fixed_height_still.url)
-                gif.attr("data-animate", results[i].images.fixed_height.url);
-                gif.attr("data-state","still");
+                var gif = $("<img>").addClass("gif");
+                gif.attr("src", results[i].images.fixed_height_still.url);
+                gif.attr("data-still",results[i].images.fixed_height_still.url);
+                gif.attr("data-animate",results[i].images.fixed_height.url)
+                gif.attr("data-state","still")
                 gifDiv.prepend(p);
                 gifDiv.prepend(gif);
                 $("#gifResults").prepend(gifDiv);
@@ -32,25 +32,24 @@ $(document).ready(function () {
     $submit.on("click", function (event) {
         event.preventDefault();
         var inputVal = $input.val();
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            inputVal + "&api_key=45zxSKlTqQdhczN4lZ9kDpkakQAQoWj1&limit=10&rating=g";
-            searches.push(inputVal);
-        var buttn = $("<button>");
-        buttn.attr("val", inputVal).addClass("userSearch");
-        buttn.text(inputVal)
-        $("#buttons").append(buttn);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + inputVal + "&api_key=45zxSKlTqQdhczN4lZ9kDpkakQAQoWj1&limit=10&rating=g";
+        searches.push(inputVal);
+        var button = $("<button>");
+        button.attr("val", inputVal).addClass("userSearch");
+        button.text(inputVal)
+        $("#buttons").append(button);
         gifSearch(queryURL);
     });
 
     $("body").on("click",".userSearch", function () {
-       var buttnVal = $(this).attr("val");
+       var buttonVal = $(this).attr("val");
        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-       buttnVal + "&api_key=45zxSKlTqQdhczN4lZ9kDpkakQAQoWj1&limit=10&rating=g";
+       buttonVal + "&api_key=45zxSKlTqQdhczN4lZ9kDpkakQAQoWj1&limit=10&rating=g";
        gifSearch(queryURL);
     })
 
 
-    // pausing gif code
+    // function to change state of gif
     var state = $(this).attr("data-state");
     $("body").on("click", ".gif", function() {
         if (state === "still") {
@@ -61,6 +60,22 @@ $(document).ready(function () {
             $(this).attr("data-state", "still");
         }
       });
+
+      function changeState(){
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      };
+
+    $(document).on("click", ".gif", changeState);
+
+
+
 
 
 });
